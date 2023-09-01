d9e1017fd12413b328f1e7db0717ef7037a521e1
suecheffy <nohheesu@gmail.com>
2023-09-02 09:24:38 +1000

    repeatString while loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  if (times < 0) return "ERROR";
+  let repeatedString = "";
 
+  while (times > 0) {
+    repeatedString += string;
+    times--;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
