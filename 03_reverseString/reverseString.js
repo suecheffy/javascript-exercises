17ac7a79bd2d10b23c9d096c9dd759195b408c18
suecheffy <nohheesu@gmail.com>
2023-09-06 09:10:10 +1000

    Reversed string JS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+function reversingString(str) {
+  // empty string
+  let aNewString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    aNewString += str[i];
+  }
+  return aNewString;
+}
 
-};
+// take input from the user
+const string = prompt("Enter a string: ");
 
-// Do not edit below this line
-module.exports = reverseString;
+const result = reversingString(string);
+console.log(result);
